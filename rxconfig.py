1d370da5f317b28693a9cd7f4c62b6064787aea2
Alexis-26 <alexis.miros@uabc.edu.mx>
2025-08-28 11:49:00 -0700

    update

--- rxconfig.py
+++ rxconfig.py
@@ -2,7 +2,6 @@ import reflex as rx
 
 config = rx.Config(
     app_name="Gestion_PosgradoFCA_Usuario",
-    telemetry_enabled=False,
     frontend_port=3000,
     backend_port=8000,
     db_url="mysql+pymysql://root:GWqLlujpGEFqvQnhLspoIldtXyFDlZxm@autorack.proxy.rlwy.net:31857/Gestion_PosgradoDB",

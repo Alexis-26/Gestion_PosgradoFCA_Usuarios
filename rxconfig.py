f19d5e143db01ea81ae9e687a4bb67c67d8ebd0f
Alexis-26 <alexis.miros@uabc.edu.mx>
2025-08-28 03:30:19 -0700

    config

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,9 @@ import reflex as rx
 
 config = rx.Config(
     app_name="Gestion_PosgradoFCA_Usuario",
+    telemetry_enabled=False,
+    frontend_port=3000,
+    backend_port=8000,
     db_url="mysql+pymysql://root:GWqLlujpGEFqvQnhLspoIldtXyFDlZxm@autorack.proxy.rlwy.net:31857/Gestion_PosgradoDB",
     api_url="https://gestionposgradofcausuarios-production.up.railway.app"
 )

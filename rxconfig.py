150f8caeb94288f1016c0ff8e6ddd23bc760e7a4
Alexis-26 <alexis.miros@uabc.edu.mx>
2025-08-28 03:24:31 -0700

    update

--- rxconfig.py
+++ rxconfig.py
@@ -3,5 +3,5 @@ import reflex as rx
 config = rx.Config(
     app_name="Gestion_PosgradoFCA_Usuario",
     db_url="mysql+pymysql://root:GWqLlujpGEFqvQnhLspoIldtXyFDlZxm@autorack.proxy.rlwy.net:31857/Gestion_PosgradoDB",
-    api_url="http://gestionposgradofcausuarios-production.up.railway.app"
+    api_url="https://gestionposgradofcausuarios-production.up.railway.app"
 )
